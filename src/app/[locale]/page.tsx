3053aa680c0cf14693fbd26c52e6ad47bbf8b3b6
tranminhwang <j2team.tranminhquang@gmail.com>
2023-07-05 00:31:44 +0700

    add custom media queries and vars scss

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,12 @@
+'use client';
+
+import { useTranslations } from 'next-intl';
+
 export default async function Home() {
+  const t = useTranslations('HomePage');
   return (
     <main className="min-h-screen">
-      <h1>Home Page</h1>
+      <h1>{t('helloWorld')}</h1>
     </main>
   );
 }
