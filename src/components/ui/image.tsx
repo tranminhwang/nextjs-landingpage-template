950804a874b61ea3bc9a8e1550b486c6a30d0cda
tranminhwang <j2team.tranminhquang@gmail.com>
2023-07-05 18:17:52 +0700

    improve image component

--- src/components/ui/image.tsx
+++ src/components/ui/image.tsx
@@ -4,22 +4,25 @@ import classNames from 'classnames';
 
 type ImageProps = React.ComponentProps<typeof NextImage>;
 
-const Image = ({ className, ...props }: ImageProps) => {
+const Image = ({
+  isInContainer = false,
+  className,
+  ...props
+}: ImageProps & {
+  isInContainer?: boolean;
+}) => {
   const ref = useRef<HTMLDivElement>(null);
 
   return (
     <div
       ref={ref}
-      className="inline-block animate-pulse bg-gray-200 rounded-lg"
-      style={{
-        height: props.height,
-        width: props.width,
-      }}>
+      className="inline-block animate-pulse bg-gray-200 rounded-lg">
       <NextImage
         {...props}
         style={{ height: props.height }}
         className={classNames(
           className,
+          isInContainer && 'w-full',
           `object-cover rounded-lg transition-opacity opacity-0 duration-[1.5s] esease-in-out`,
         )}
         onLoadingComplete={(image) => {
