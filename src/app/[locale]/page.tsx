1577b1cfd1449026aea2f1358009c758611543ec
tranminhwang <j2team.tranminhquang@gmail.com>
2023-07-05 17:29:25 +0700

    add skeleton loader to Image component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -23,7 +23,6 @@ export default async function Home() {
           height={192}
           src="https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1024&h=1280&q=80"
           alt="a"
-          className={'rounded-md'}
         />
         <HeadingOne
           className={classNames(
--- src/components/ui/image.tsx
+++ src/components/ui/image.tsx
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import NextImage from 'next/image';
 import classNames from 'classnames';
 
 type ImageProps = React.ComponentProps<typeof NextImage>;
 
 const Image = ({ className, ...props }: ImageProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+
   return (
-    <NextImage
-      {...props}
+    <div
+      ref={ref}
+      className="inline-block animate-pulse bg-gray-200 rounded-lg"
       style={{
         height: props.height,
         width: props.width,
-      }}
-      className={classNames(className, `object-cover`)}
-    />
+      }}>
+      <NextImage
+        {...props}
+        style={{ height: props.height }}
+        className={classNames(
+          className,
+          `object-cover rounded-lg transition-opacity opacity-0 duration-[1.5s] esease-in-out`,
+        )}
+        onLoadingComplete={(image) => {
+          image.classList.remove('opacity-0');
+          ref.current?.classList.remove(
+            'animate-pulse',
+            'bg-gray-200',
+            'rounded-lg',
+          );
+          ref.current?.removeAttribute('style');
+        }}
+      />
+    </div>
   );
 };
 
